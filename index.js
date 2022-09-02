dfe710a2fd756f7a3652c0f1c63c217339d65300
idhaya780 <idhaya780@gmail.com>
2022-09-02 13:54:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//  alert("javascript course");
+ var data = "anbu";
+ var msg = "welcome javascript  " + data + "  this is myname";
+ document.getElementById("demo").innerText=msg;
+ console.log(msg);
+
+//  document.getElementById("demo").innerHTML="do something";
+
+// let x = "john";
+// y = "duo";
+// z = x + y;
+//  document.getElementById('demo').innerHTML = "my name is  "  + z  
+
+// // const car = ["volo", "bmw" ,"audi"]
+
+// // car[0]="toyoto";
+// // car.push('suzuki')
+// const car = {type:"Fiat", model:"500", color:"white"};
+
+// // You can change a property:
+// car.color = "red";
+
+// // You can add a property:
+// car.owner = "Johnson";
+
+// document.getElementById('demo').innerHTML = car.type +" " + car.model + " " + car.color;
+
+
